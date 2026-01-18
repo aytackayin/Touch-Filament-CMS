9c51b4bc59b235e81b30417af1497d475156e893
Aytaç KAYIN <aytackayin@gmail.com>
2026-01-18 05:03:09 +0300

    cropper aspectler eklendi

--- app/Filament/Resources/TouchFileManager/Schemas/TouchFileForm.php
+++ app/Filament/Resources/TouchFileManager/Schemas/TouchFileForm.php
@@ -83,7 +83,9 @@ class TouchFileForm
                                     ->imageEditorAspectRatios([
                                         null,
                                         '16:9',
+                                        '9:16',
                                         '4:3',
+                                        '3:4',
                                         '1:1',
                                     ])
                                     ->enableReordering()
@@ -107,7 +109,9 @@ class TouchFileForm
                                     ->imageEditorAspectRatios([
                                         null,
                                         '16:9',
+                                        '9:16',
                                         '4:3',
+                                        '3:4',
                                         '1:1',
                                     ])
                                     ->preserveFilenames()
