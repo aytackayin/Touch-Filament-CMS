ddb03531ddf24de0f470042b4caf28779ee023ae
Aytaç KAYIN <aytackayin@gmail.com>
2026-01-19 14:37:39 +0300

    icon select eksiksiz çalışıyor.

--- app/Filament/Pages/ManageSiteSettings.php
+++ app/Filament/Pages/ManageSiteSettings.php
@@ -125,9 +125,11 @@ class ManageSiteSettings extends SettingsPage
                             ->options(collect(static::getIcons())->mapWithKeys(function ($label, $value) {
                                 return [$value => "<div style='display: flex; align-items: center; gap: 8px; white-space: nowrap;'> <div style='width: 20px; height: 20px; display: flex; align-items: center; justify-content: center; flex-shrink: 0;'>" . svg($value)->style('width: 20px; height: 20px;')->toHtml() . "</div> <span style='line-height: 1;'>{$label}</span></div>"];
                             })->toArray())
+                            ->optionsLimit(false)
                             ->searchable()
                             ->prefixIcon(fn($state) => (blank($state) || str_starts_with($state, 'heroicon-')) ? $state : 'heroicon-' . $state)
                             ->native(false),
+
 
 
 
